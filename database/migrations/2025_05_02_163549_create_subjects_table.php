cd9b9e9ddfcfaf82f3b7ebbe810a73290bb9ca51
Enrique Redondo Cortés <eredondoco.inf@upsa.es>
2025-05-05 13:33:52 +0200

    ✨feat: agregar relación subjects-users
    Se añadió un campo `user_id` con relación foránea a la tabla `subjects`, vinculada a la tabla `users` con eliminación en cascada. Esto permite asociar cada subject a un usuario específico.

--- database/migrations/2025_05_02_163549_create_subjects_table.php
+++ database/migrations/2025_05_02_163549_create_subjects_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->text('description');
             $table->text('extra_instructions')->nullable();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
