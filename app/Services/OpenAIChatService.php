0d90735e8f53c5d6ccab1e0e43be27873b1454c3
Enrique Redondo Cortés <eredondoco.inf@upsa.es>
2025-05-22 11:03:26 +0200

    ✨feat: enviar y recibir mensajes de API
    Se añadió funcionalidad para gestionar streams en las ejecuciones y eventos asociados dentro del servicio OpenAIChatService. Además, se introdujeron nuevos métodos para manejar mensajes y respuestas de manera personalizada, mejorando la interacción con los threads.

--- app/Services/OpenAIChatService.php
+++ app/Services/OpenAIChatService.php
@@ -7,14 +7,11 @@ use OpenAI\Client;
 
 class OpenAIChatService
 {
-    protected Client $client;
+    protected Client $cliente;
 
-    /**
-     * @param Client $client
-     */
-    public function __construct(Client $client)
+    public function __construct(Client $cliente)
     {
-        $this->client = $client;
+        $this->cliente = $cliente;
     }
 
     /**
@@ -23,10 +20,104 @@ class OpenAIChatService
     public function createThread(): string
     {
         try {
-            $request = $this->client->threads()->create([]);
+            $peticion = $this->cliente->threads()->create([]);
+            return $peticion->id;
+        } catch (Exception $e) {
+            throw new Exception("Error al crear el thread.");
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function createMessage(
+        string $thread_id,
+        string $userMessage
+    ): string
+    {
+        try {
+            $request = $this->cliente->threads()->messages()->create($thread_id, [
+                'role' => 'user',
+                'content' => $userMessage,
+            ]);
             return $request->id;
         } catch (Exception $e) {
-            throw new Exception("Error al crear la thread.");
+            throw new Exception(
+                "Error al crear el mensaje para el thread \"$thread_id\"."
+            );
         }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function executeRunAndGetResponseStream(
+        string $thread_id,
+        string $assistant_id
+    ): ?string
+    {
+        $contenidoMensaje = null;
+
+        try {
+            $stream = $this->cliente->threads()->runs()->createStreamed(
+                threadId: $thread_id,
+                parameters: [
+                    'assistant_id' => $assistant_id,
+                ]
+            );
+
+            foreach ($stream as $response) {
+                $event = $response->event;
+                $data = $response->response;
+
+                $resultado = $this->handleRunStream($event, $data);
+                if ($resultado !== null) {
+                    $contenidoMensaje = $resultado;
+                }
+            }
+
+            return $contenidoMensaje;
+        } catch (Exception $e) {
+            throw new Exception("Error en el stream de la run para el thread \"$thread_id\".");
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function handleRunStream($event, $data): ?string
+    {
+        if ($event === 'thread.message.completed') {
+            $message = $this->getMessageText($data->content);
+            return $message ?: null;
+        }
+
+        // Fail events.
+        if ($event === 'thread.run.failed')
+            throw new Exception("La ejecución del run ha fallado: " .
+                ($data->lastError?->message ?? 'Error desconocido'));
+
+        if ($event === 'thread.run.expired')
+            throw new Exception("La ejecución del run ha expirado");
+
+        if ($event === 'thread.run.cancelled')
+            throw new Exception("La ejecución del run ha sido cancelada");
+
+        if ($event === 'thread.message.incomplete')
+            throw new Exception("El mensaje está incompleto");
+
+        // Assume other events as waiting for completion.
+        return null;
+    }
+
+    private function getMessageText(array $content): string
+    {
+        $messageContent = '';
+
+        foreach ($content as $item)
+            if ($item->type === 'text')
+                $messageContent .= $item->text->value;
+
+        return $messageContent;
     }
 }
