d3856886056a0d6e999e9863a6d12d05a284634e
Enrique Redondo Cortés <eredondoco.inf@upsa.es>
2025-05-25 14:00:27 +0200

    ✨feat: agregar componente DoubtDashboard
    Se añade un nuevo componente llamado DoubtDashboard para gestionar y procesar chats pendientes de procesar por asignatura. Incluye funcionalidad para confirmar y procesar todos los chats no atendidos.

---
+++ resources/js/Pages/Subjects/Partials/DoubtDashboard.jsx
@@ -0,0 +1,29 @@
+import {router} from "@inertiajs/react";
+
+const DoubtDashboard = ({unprocessedChatsNumber, subjectId}) => {
+
+    const processAllChats = () => {
+        router.get(route('chats.process', {subjectId}), {
+            onBefore: () => confirm('¿Desea procesar todos los chats de esta asignatura ahora?')
+        })
+    }
+
+    if (unprocessedChatsNumber === 0) {
+        return (
+            <div>
+                <p>Todos los chats de esta asignatura han sido procesados.</p>
+            </div>
+        );
+    }
+    const numberText = unprocessedChatsNumber === 1 ? 'un' : unprocessedChatsNumber;
+    const chatText = unprocessedChatsNumber === 1 ? 'chat nuevo' : 'chats nuevos';
+
+    return (
+        <div>
+            <p>Hay {numberText} {chatText} sin procesar.</p>
+            <button onClick={processAllChats}>Procesar todos los chats</button>
+        </div>
+    );
+};
+
+export default DoubtDashboard;
