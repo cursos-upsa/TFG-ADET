b38a79b2ff23bf70d3e392e2aa707252f3133829
Enrique Redondo Cortés <eredondoco.inf@upsa.es>
2025-05-30 10:45:15 +0200

    ✨feat: gestionar estado al procesar chats
    Se añadió un estado para manejar el bloqueo del botón mientras se procesan los chats. Ahora se desactiva el botón y muestra un mensaje de "Procesando..." para mejorar la retroalimentación al usuario.

--- resources/js/Pages/Subjects/Partials/DoubtProcessingControl.jsx
+++ resources/js/Pages/Subjects/Partials/DoubtProcessingControl.jsx
@@ -1,11 +1,17 @@
 import {router} from "@inertiajs/react";
+import {useState} from "react";
 
 const DoubtProcessingControl = ({unprocessedChatsNumber, subjectId}) => {
+    const [processing, setProcessing] = useState(false)
 
     const processAllChats = () => {
-        router.get(route('chats.process', {subjectId}), {
-            onBefore: () => confirm('¿Desea procesar todos los chats de esta asignatura ahora?')
-        })
+        router.get(route('chats.process', {subjectId}),
+            {},
+            {
+                onBefore: () => confirm('¿Desea procesar todos los chats de esta asignatura ahora?'),
+                onStart: () => setProcessing(true),
+                onFinish: () => setProcessing(false),
+            })
     }
 
     if (unprocessedChatsNumber === 0) {
@@ -21,7 +27,11 @@ const DoubtProcessingControl = ({unprocessedChatsNumber, subjectId}) => {
     return (
         <div>
             <p>Hay {numberText} {chatText} sin procesar.</p>
-            <button onClick={processAllChats}>Procesar todos los chats</button>
+            <button onClick={processAllChats} disabled={processing}>
+                {processing ?
+                    'Procesando...' :
+                    'Procesar todos los chats'}
+            </button>
         </div>
     );
 };
